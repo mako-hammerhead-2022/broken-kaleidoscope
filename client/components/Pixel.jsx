0447a2c27f3640530b675d622f643db7c5b59432
Adam Chin <103253312+adam-chin1996@users.noreply.github.com>
2022-06-14 11:27:37 +1200

    rainbow pixels

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -5,10 +5,10 @@ const Pixel = () => {
     const [style, setStyle] = useState({
     height: '50px',
     width: '50px',
-    backgroundColor: '#7fffd4',
+    backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`,
   })
   return <div style={style}>  </div>
 }
 
-
+// 
 export default Pixel
