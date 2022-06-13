1ec9708498819b5eb73ec3081e08fbea8ae29200
Rafael <103238522+rafaelavb@users.noreply.github.com>
2022-06-14 11:12:58 +1200

    Added first div

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+const Pixel = () => {
+  return <>
+      <div style={{
+    height: 100,
+    width:100,
+    backgroundColor: "blue"
+  }}> 
+
+  </div>
+  </>
+
+}
+export default Pixel
