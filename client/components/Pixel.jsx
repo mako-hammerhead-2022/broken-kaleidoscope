6b4ce2152a0d15d5a0bee11caddb40e8190d8ea4
Jojo Mepham <103151844+Jojo-Mepham@users.noreply.github.com>
2022-06-14 06:59:04 +0800

    add first pixel

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function Pixel(props) {
-  return (<div>pixel div</div>)
+  return (<div style ={{
+    height: '50px',
+    width: '50px',
+    backgroundColor: 'turquoise'
+  }}></div>)
 }
 
 export default Pixel
