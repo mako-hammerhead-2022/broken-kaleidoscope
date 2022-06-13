582c1ef09f486d3b522d24c0267527cb5a85f3bf
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-06-14 11:27:41 +1200

    added pixelcolor randomizer

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,11 +1,24 @@
 import React, {useState} from 'react'
 
+
 const Pixel = () => {
+
+  const randomColour = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+  const pixelRandomizer = {height: '5rem', width: '5rem', backgroundColor: randomColour()}
   //this is where you do your state stuff and set the initial state.
-  const [pixelStyle, setPixelStyle] = useState({height: '5rem', width: '5rem', backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`})
+  const [pixelStyle, setPixelStyle] = useState(pixelRandomizer)
   
+  const changeColor = (evt) => {
+    console.log(evt.target)
+    setPixelStyle(
+      {...pixelStyle, backgroundColor: randomColour()}
+    )
+  }
+
+
   return (
-  <div style={pixelStyle}/>
+  <div style={pixelStyle} onClick={changeColor}/>
   )
 }
 
