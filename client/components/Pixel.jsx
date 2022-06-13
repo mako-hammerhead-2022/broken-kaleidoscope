d235ed155d48fc4775011862e6bf6ff41bddc96e
Louis Nguyen <103094279+louis-nguyen108@users.noreply.github.com>
2022-06-14 11:03:22 +1200

    update backgroundColour to random colours

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -2,8 +2,12 @@ import React, {useState} from 'react'
 
 
 function Pixel () {
-  const [style, setStyle] = useState({fontFamily: 'Times New Roman',height: '100px',width: '100px',backgroundColor: 'cornflowerblue'})
-
+  const [style, setStyle] = useState({
+    fontFamily: 'Times New Roman',
+    height: '100px',
+    width: '100px',
+    backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+  })
 
   return (
     <div style={style}></div>
