ea543a7929f00d84908e9e2525af49bd4a796a15
Mat Ruane <8108538+matruane@users.noreply.github.com>
2022-06-14 11:06:02 +1200

    In a state of flux

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -4,9 +4,12 @@ import React, { useState } from "react";
 const Pixel = () => {
   const [style, setStyle] = useState({height: '50px', width: '50px', backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}` })
 
+  const clickHandler = (evt) => {
+    setStyle({height: '50px', width: '50px', backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}` })
+  }
 
   return (
-  <div style={style}>
+  <div style={style} onClick={clickHandler}>
   </div>
   )  
 }
