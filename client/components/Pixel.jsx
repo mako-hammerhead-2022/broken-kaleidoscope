b9157e3a92f84f9b015d3d0108111e5dc974c1f8
tim-dustow <103151914+tim-dustow@users.noreply.github.com>
2022-06-14 11:01:30 +1200

    Rainbow pixel

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 
 const Pixel = () => {
-  const [style, setStyle] = useState({height: '50px', width: '50px', backgroundColor: 'red' })
+  const [style, setStyle] = useState({height: '50px', width: '50px', backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}` })
 
 
   return (
