8e3adef158d8facb4f4244c1a1ba9da4b89e52ed
beyond_wen_mako_22 <103220308+Beyond-Wen@users.noreply.github.com>
2022-06-14 12:38:24 +1200

    set usestate

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react'
 
 const Pixel = () => {
+  const [colour, setColour] = useState('blue')
+
+  function handleClick() {
+    setColour('yellow')
+  }
+
   return (
-    <div style={{
-      height: '20px',
-      width: '20px',
-      backgroundColor: 'orange',
-    }} >
-      
-    </div>
-  ) 
+    <div
+      style={{
+        height: '20px',
+        width: '20px',
+        backgroundColor: colour,
+      }}
+      onClick={handleClick}
+    ></div>
+  )
 }
 
 export default Pixel
