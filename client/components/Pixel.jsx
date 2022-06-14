6cf43887a02ffc6765c7550827f5320c4f2cfcfc
Jojo Mepham <103151844+Jojo-Mepham@users.noreply.github.com>
2022-06-14 12:26:10 +0800

    random pixel onClick

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, {useState} from 'react'
 
 
@@ -11,14 +13,23 @@ import React, {useState} from 'react'
 
 function Pixel() {
   function random () {
-    return Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)
+    return `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
   }
-  const pixelStyle = {height: '5vw',
-  width: '5vw',
-  backgroundColor: `#${random()}`}
-  const [style, setStyle] = useState(pixelStyle)
+  function randomPixel () {
+    return {height: '5vw',
+    width: '5vw',
+    backgroundColor: random()}
+  }
+  // const pixelStyle = {height: '5vw',
+  // width: '5vw',
+  // backgroundColor: `#${random()}`}
+  const [style, setStyle] = useState(randomPixel())
+  const changePixel = () => {
+    const color = random()
+    setStyle({...style, backgroundColor: color})
+   }
 
-  return (<div style={style} onClick={() => setStyle(pixelStyle)}></div>)
+  return (<div style={style} onClick={changePixel}></div>)
 }
 
 // onClick={changePixel}
