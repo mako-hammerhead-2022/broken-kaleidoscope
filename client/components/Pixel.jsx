92f5323fe760d51286c4c7f964daadfb540a676a
ayoung-lee <103156367+ayoung-lee@users.noreply.github.com>
2022-06-14 12:58:25 +1200

    finish up to random colour

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,11 +1,20 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react'
 
 const Pixel = () => {
-  const [colour, setColour] = useState('blue')
+  const [colour, setColour] = useState(
+    `#${Math.floor(Math.random() * 0x1000000)
+      .toString(16)
+      .padStart(6, 0)}`
+  )
 
   function handleClick() {
-    setColour('yellow')
+    setColour(
+      `#${Math.floor(Math.random() * 0x1000000)
+        .toString(16)
+        .padStart(6, 0)}`
+    )
   }
 
   return (
