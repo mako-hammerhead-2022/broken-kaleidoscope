b2ba7505fbe94d0710f7a398a12b91deeda43397
Jojo Mepham <103151844+Jojo-Mepham@users.noreply.github.com>
2022-06-14 08:13:40 +0800

    placed random background color in function

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Pixel(props) {
-  return (<div style ={{
-    height: '5vw',
-    width: '5vw',
-    backgroundColor: 'turquoise'
-  }}></div>)
+
+// const changePixel = (evt) => {
+//  setStyle(style)
+// }
+
+// function Pixel() {
+//   return (<div style={style} onClick={() =>  setStyle(style)}></div>)
+// }
+
+function Pixel() {
+  function random () {
+    return Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)
+  }
+  const pixelStyle = {height: '5vw',
+  width: '5vw',
+  backgroundColor: `#${random()}`}
+  const [style, setStyle] = useState(pixelStyle)
+
+  return (<div style={style} onClick={() => setStyle(pixelStyle)}></div>)
 }
+
+// onClick={changePixel}
 
 export default Pixel
