989a4307bc3958bc01597d88b0e8d15d6d9c32ec
Jojo Mepham <103151844+Jojo-Mepham@users.noreply.github.com>
2022-06-14 08:16:47 +0800

    made new branch

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -10,13 +10,18 @@ import React, {useState} from 'react'
 // }
 
 function Pixel() {
+  
   function random () {
     return Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)
   }
+  
   const pixelStyle = {height: '5vw',
   width: '5vw',
   backgroundColor: `#${random()}`}
+
   const [style, setStyle] = useState(pixelStyle)
+
+  
 
   return (<div style={style} onClick={() => setStyle(pixelStyle)}></div>)
 }
