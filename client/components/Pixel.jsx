8c77cc614e6340058c8937992a3445044b14c8da
Jojo Mepham <103151844+Jojo-Mepham@users.noreply.github.com>
2022-06-14 15:47:40 +0800

    onContextMenu success

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -26,7 +26,12 @@ const Pixel = () => {
     setStyle({...style, backgroundColor: 'green'})
   }
 
-  return (<div style={style} onClick={changePixel} onMouseEnter={greenPixel}></div>)
+  const blackPixel = (evt) => {
+    evt.preventDefault()
+    setStyle({...style, backgroundColor: 'black'})
+  }
+
+  return (<div style={style} onClick={changePixel} onMouseEnter={greenPixel} onContextMenu={blackPixel}></div>)
   }
 
 export default Pixel
