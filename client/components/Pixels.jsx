da75adeadbedcb332b47ea76a62da4d02d08a3ca
Rafael <103238522+rafaelavb@users.noreply.github.com>
2022-06-14 17:02:31 +1200

    added contextMenu function to make squares turn black with a right click

--- client/components/Pixels.jsx
+++ client/components/Pixels.jsx
@@ -8,20 +8,30 @@ const Pixels = () => {
     backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
   })
 
-    //added onMouseEnter function 
+    //added onContextMenu function for right click to turn squares black
+     const contextMenu = (evt) => {
+      evt.preventDefault()
+      setStyle({height: '100px', width: '100px', backgroundColor: 'black' })
+     }
+
+
+    //added onMouseEnter function that when you hover over squares they turn green 
     const mouseHandler = (evt) => {
       setStyle({height: '100px', width: '100px', backgroundColor: 'green'})
     }
 
 
-  //added onclick handler function 
+  //added onclick handler function so when you click they switch colours
   const clickHandler = (evt) => {
     setStyle({height: '100px', width: '100px', backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}` 
   })
   }
 
   return (
-      <div style={style} onClick={clickHandler} onMouseEnter={mouseHandler}> 
+      <div style={style}
+       onClick={clickHandler}
+        onMouseEnter={mouseHandler}
+         onContextMenu={contextMenu}> 
       </div>
 
       )
