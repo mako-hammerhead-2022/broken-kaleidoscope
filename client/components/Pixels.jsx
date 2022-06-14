133ba95379091cd167c24d48849aa16c87d9a5ec
Rafael <103238522+rafaelavb@users.noreply.github.com>
2022-06-14 16:49:10 +1200

    broke code again and refixed it

--- client/components/Pixels.jsx
+++ client/components/Pixels.jsx
@@ -7,39 +7,23 @@ const Pixels = () => {
     width: '100px',
     backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
   })
-   
+
+
   //added onclick handler function 
-    const clickHandler = evt => ({ 
-       backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-    })
+  const clickHandler = (evt) => {
+    setStyle({height: '100px', width: '100px', backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}` 
+  })
+  }
+
+
 
   return (
-      <div  style={style} onClick={clickHandler}> </div>
-      //returned onclick handler function 
-    )
+      <div style={style} onClick={clickHandler}> 
+      </div>
+
+      )
     }
 
     
-
-  // function handleClick(evt) {
-  //   setStyle(style + increment)
-  // }
-  
-  // function handleChange(evt) {
-  //   console.log(evt.target.value)
-  //   setIncrement(evt.target.value)
-  // }
-  
-  // return(
-  //   <>
-    
-  //   <div>Style is {style}</div>
-  
-  //   <input type='number' onChange={handleChange} value={increment}/>
-  //     <button onClick={handleClick}>Number go up</button>
-  //   </>
-  // )
-  
-  
 
 export default Pixels
