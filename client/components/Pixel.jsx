da8c9dbdd5db5913ab66a81aed8ade17e37783ef
charlotte-greatbatch <charlottegreatbatch01@gmail.com>
2022-06-26 16:43:28 +1200

    pixels w/ state

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Pixel = () =>  {
+
+  const [style, setStyle] = useState({
+    height: '10px',
+    width: '10px',
+    backgroundColor: '#9E788F'
+  })
   return (
-    <div style={{
-      height: '10px',
-      width: '10px',
-      backgroundColor: '#9E788F',
-    }}></div>
+<div style={style}></div>
   )
 }
 
