910fab22353b6e3416e7225fa75ef785efc93851
charlotte-greatbatch <charlottegreatbatch01@gmail.com>
2022-06-26 16:39:12 +1200

    purp pixel on page

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pixel = () =>  {
+  return (
+    <div style={{
+      height: '10px',
+      width: '10px',
+      backgroundColor: '#9E788F',
+    }}></div>
+  )
+}
+
+export default Pixel
